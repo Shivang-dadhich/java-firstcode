970efb3d68d028697fa5b06a9f9037fd986b32b1
shivang-dadhich <shivangdadhich344@gamil.com>
2024-12-08 22:40:32 +0530

    print hello world by java

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # java-firstcode
 this repo is only for prectice java and git.
+i print hello world by java .
+
---
+++ problem.java
@@ -0,0 +1,6 @@
+import java.lang.*;
+class problem{
+    public static void main(String []args){
+        System.out.print("hello world");
+    }
+}
